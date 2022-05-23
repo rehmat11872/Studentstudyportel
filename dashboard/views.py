9219c5607870e4328c2aa13b1e35893ae1fdb0a4
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-05-23 18:04:36 +0000

    profile page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,4 @@
+from certifi import contents
 from django.shortcuts import redirect, render
 from .models import *
 from .forms import *
@@ -339,3 +340,24 @@ def register(request):
             'form':form,
         }
     return render(request, 'dashboard/register.html', context)
+
+
+def profile(request):
+    homeworks = Homework.objects.filter(is_finished=False, user=request.user)
+    todos = Todo.objects.filter(is_finished=False, user=request.user)
+    if len(homeworks) == 0:
+        homework_done = True
+    else:
+        homework_done = False
+    if len(todos) == 0:
+        todos_done = True
+    else:
+        todos_done = False
+    context = {
+        'homeworks':homeworks,
+        'todos':todos,
+        'homework_done':homework_done,
+        'todos_done':todos_done
+    }    
+    return render(request, 'dashboard/profile.html', context)
+
