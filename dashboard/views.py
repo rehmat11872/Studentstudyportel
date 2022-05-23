0203e15ef3627cf1c986455dabb2515f0f719c74
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-05-23 17:45:55 +0000

    Login

--- dashboard/views.py
+++ dashboard/views.py
@@ -332,6 +332,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f"Account Created for {username}!!")
+            return redirect('login')
     else:        
         form = UserRegistrationForm()
     context = {
